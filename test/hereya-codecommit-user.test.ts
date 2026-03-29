eaeab0f03439207e41247f6abdc1b26c74a5a3a9
Jonatan Sombié <jonatan.sombie@gmail.com>
2026-03-29 12:27:48 +0200

    Initial commit

---
+++ test/hereya-codecommit-user.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HereyaCodecommitUser from '../lib/hereya-codecommit-user-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hereya-codecommit-user-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HereyaCodecommitUser.HereyaCodecommitUserStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
